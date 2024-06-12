0cecad49ceb9c18f6ae563c6e5ba2038f1314d87
EfiokobongN <mailpreciousnelson@gmail.com>
2024-06-12 21:15:04 +0000

    built command run

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routes rou
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
